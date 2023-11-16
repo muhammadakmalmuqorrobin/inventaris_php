7dbc9b1e83f6644934487f97e8445be5cb79c650
muhammadakmalmuqorrobin <150991763+muhammadakmalmuqorrobin@users.noreply.github.com>
2023-11-16 12:45:00 +0700

    Add files via upload

---
+++ inventaris/page/home.php
@@ -0,0 +1,5 @@
+<div class="jumbotron">
+        <h1>Selamat Datang, <?= $user ?>!</h1>
+        <p>Selamat Datang di Aplikasi Inventaris Kantor</p>
+        <p>Aplikasi ini merupakan aplikasi untuk uji level kelas XII RPL</p>
+      </div>
